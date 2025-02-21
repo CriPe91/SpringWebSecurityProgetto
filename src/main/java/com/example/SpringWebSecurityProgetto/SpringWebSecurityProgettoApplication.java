a60683fe205b29a80792c36d5a83132b291cd50c
CriPe91 <cristian.petta91@gmail.com>
2025-02-21 15:13:11 +0100

    Primo commit

---
+++ src/main/java/com/example/SpringWebSecurityProgetto/SpringWebSecurityProgettoApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringWebSecurityProgetto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebSecurityProgettoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebSecurityProgettoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringWebSecurityProgetto/SpringWebSecurityProgettoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringWebSecurityProgetto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebSecurityProgettoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
